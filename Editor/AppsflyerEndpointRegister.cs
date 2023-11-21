02b0336c0e4b0e414490b4314af2931a629eb1ae
trungnc <trungnc01@jacat.games>
2023-11-21 17:41:22 +0700

    format code

--- Editor/AppsflyerEndpointRegister.cs
+++ Editor/AppsflyerEndpointRegister.cs
@@ -14,23 +14,23 @@ namespace Omnilatent.AppsFlyerWrapperNS.EditorNS
         public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
         {
 #if UNITY_IOS
-        if (target == BuildTarget.iOS)
-        {
-            string plistPath = pathToBuiltProject + "/Info.plist";
-            PlistDocument plist = new PlistDocument();
-            plist.ReadFromString(File.ReadAllText(plistPath));
+            if (target == BuildTarget.iOS)
+            {
+                string plistPath = pathToBuiltProject + "/Info.plist";
+                PlistDocument plist = new PlistDocument();
+                plist.ReadFromString(File.ReadAllText(plistPath));
 
-            PlistElementDict rootDict = plist.root;
-            rootDict.SetString("NSAdvertisingAttributionReportEndpoint", "https://appsflyer-skadnetwork.com/");
-    
-            /*** To add more keys :
-             ** rootDict.SetString("<your key>", "<your value>");
-             ***/
+                PlistElementDict rootDict = plist.root;
+                rootDict.SetString("NSAdvertisingAttributionReportEndpoint", "https://appsflyer-skadnetwork.com/");
 
-            File.WriteAllText(plistPath, plist.WriteToString());
+                /*** To add more keys :
+                 ** rootDict.SetString("<your key>", "<your value>");
+                 ***/
 
-            Debug.Log("Info.plist updated with NSAdvertisingAttributionReportEndpoint");
-        }
+                File.WriteAllText(plistPath, plist.WriteToString());
+
+                Debug.Log("Info.plist updated with NSAdvertisingAttributionReportEndpoint");
+            }
 #endif
         }
     }
